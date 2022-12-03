8e169cdf08dbd0cfe5ad6df5e66e3cf137856906
chuongduong <duongthuychuong@gmail.com>
2022-12-03 22:41:44 +0700

    increase and decrease item

--- src/reducer.js
+++ src/reducer.js
@@ -8,6 +8,24 @@ const reducer = (state, action) => {
   if (action.type === "CLEAR_CART") {
     return { ...state, cart: [] };
   }
+  if (action.type === "INCREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount + 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
+  if (action.type === "DECREASE") {
+    let tempCart = state.cart.map((item) => {
+      if (item.id === action.payload) {
+        return { ...item, amount: item.amount === 0 ? 0 : item.amount - 1 };
+      }
+      return item;
+    });
+    return { ...state, cart: tempCart };
+  }
   return state;
 };
 
